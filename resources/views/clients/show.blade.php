8194dbfe4c2fc593a6ed4f65389ec473650764f0
joelohr01 <joe@joelohrdesign.com>
2016-09-18 13:00:54 -0500

    Updates to Client Detail Page

--- resources/views/clients/show.blade.php
+++ resources/views/clients/show.blade.php
@@ -5,14 +5,23 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Dashboard</div>
+                    <div class="panel-heading">{{ $client->clientname }} - Details</div>
 
                     <div class="panel-body">
-                        <table class="table">
-                            <tr>
-                                <td>{{ $client->clientname }}</td>
-                            </tr>
-                        </table>
+                        <div class="row">
+                            <div class="col-md-8">
+                                <h1>Company Details</h1>
+                            </div>
+                            <div class="col-md-4">
+                                <h1>{{ $client->clientname }}</h1>
+                                <address>
+                                    {{ $client->address }}<br>
+                                    {{ $client->city }}, {{ $client->state }} {{ $client->zipcode }}
+                                </address>
+                                <p><a href="tel:{{ $client->phone }}">{{ $client->phone }}</a></p>
+                                <p><a href="mailto:{{ $client->email }}">{{ $client->email }}</a></p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
